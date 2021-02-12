ae203580644cedf58fd3c2841ef8cbb147520b8d
Paulo Ferreira <phgferreira@outlook.com>
2021-02-12 20:59:47 -0300

    Spring Initializr

---
+++ src/main/java/br/com/bbl/fantasyfable/FantasyFableApplication.java
@@ -0,0 +1,13 @@
+package br.com.bbl.fantasyfable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyFableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyFableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bbl/fantasyfable/FantasyFableApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bbl.fantasyfable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyFableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
